68b74209e9e9cc5a435fd3693e42a1ade5ede983
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-09-04 20:20:24 +0500

    footer build with logo

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import Head from "next/head";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>ZASFarmStore</title>
-        <meta
-          name="description"
-          content="ZASFarmStore- Farm fresh organic food "
-        />
+        <meta name="description" content="ZASFarmStore- Farm fresh organic " />
       </Head>
-      this is me
-      <div className="mx-6  ">ZASFarmStore</div>
+      <Navbar />
+      <div>
+        <img src="/Farm_FRESH.jpeg" alt="" />
+      </div>
+      <Footer />
     </div>
   );
 }
