b2062c0005096e58850c59683020ad816cf5fb94
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-09-06 23:12:41 +0500

    navbar complete

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ const footer = () => {
   return (
     <div>
       <footer className="text-gray-600 body-font">
-        <div className="container px-5 py-12 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+        <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
           <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
             <Link href={"/"}>
               <a className="flex title-font font-medium py-2 items-center md:justify-start justify-center text-gray-900">
@@ -19,7 +19,7 @@ const footer = () => {
             </Link>
             <p className="mx-10 text-sm text-gray-500">Farm fresh organic</p>
           </div>
-          <div className="flex-grow flex flex-wrap py-12 md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+          <div className="flex-grow flex flex-wrap   md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
                 CATEGORIES
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,58 @@
 import React from "react";
-
+import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 const navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <div className="flex flex-col md:flex-row justify-center md:justify-start items-center ">
+      <div className="logo mx-5">
+        <Image
+          src="/logo.png"
+          alt="ZASFarmStore Farm fresh organic"
+          width={100}
+          height={100}
+        />
+      </div>
+      <div className="nav">
+        <ul className="flex items-center text-lg space-x-2 font-bold sm:text-xl sm:space-x-4 md:text-2xl md:space-x-6">
+          <Link href={"/"}>
+            <a>
+              <li>Eggs</li>
+            </a>
+          </Link>
+          <Link href={"/"}>
+            <a>
+              <li>Friuts</li>
+            </a>
+          </Link>
+          <Link href={"/"}>
+            <a>
+              <li>Meat</li>
+            </a>
+          </Link>
+          <Link href={"/"}>
+            <a>
+              <li>Milk</li>
+            </a>
+          </Link>
+          <Link href={"/"}>
+            <a>
+              <li>Vegetables</li>
+            </a>
+          </Link>
+
+          <Link href={"/"}>
+            <a>
+              <li>Others</li>
+            </a>
+          </Link>
+        </ul>
+      </div>
+      <div className="cart absolute top-8 right-6 mx-5">
+        <AiOutlineShoppingCart className="text-2xl   md:text-4xl" />
+      </div>
+    </div>
+  );
 };
 
 export default navbar;
