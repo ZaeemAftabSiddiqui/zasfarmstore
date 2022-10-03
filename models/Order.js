678133d0e9fc1a5934ab5d1ec3d4e7da0656bc93
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-10-03 14:25:32 +0500

    Add MongoDB Database

---
+++ models/Order.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: { type: String, require: true },
+    products: [
+      {
+        productId: { type: String },
+        quantity: { type: Number, default: 1 },
+      },
+    ],
+    address: { type: String, require: true },
+    amount: { type: Number, require: true },
+    status: { type: String, default: "pending", require: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Order", OrderSchema);
---
+++ models/product.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    title: { type: String, require: true },
+    slug: { type: String, require: true, unique: true },
+    desc: { type: String, require: true },
+    img: { type: String, require: true },
+    category: { type: String, require: true },
+    size: { type: String },
+    color: { type: String },
+    price: { type: Number, require: true },
+    availableQty: { type: String, require: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", ProductSchema);
