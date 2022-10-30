93b7344f03705fbca12aac497f873baa399979fc
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-10-30 12:23:50 +0500

    Adding Encryption to login and signup

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,14 +1,17 @@
 import connentDb from "../../middleware/mongoose";
 import User from "../../models/User";
+var CryptoJS = require("crypto-js");
 
 const handler = async (req, res) => {
   if (req.method === "POST") {
     console.log(req.body);
     let user = await User.findOne({ email: req.body.email });
+    const bytes = CryptoJS.AES.decrypt(user.password, "secret key 123");
+    let decryptedpass = bytes.toString(CryptoJS.enc.Utf8);
     if (user) {
       if (
         req.body.email === user.email &&
-        req.body.password === user.password
+        req.body.password === decryptedpass
       ) {
         res
           .status(200)
--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,10 +1,19 @@
 import connentDb from "../../middleware/mongoose";
 import User from "../../models/User";
+var CryptoJS = require("crypto-js");
 
 const handler = async (req, res) => {
   if (req.method === "POST") {
     console.log(req.body);
-    let u = new User(req.body);
+    const { name, email } = req.body;
+    let u = new User({
+      name,
+      email,
+      password: CryptoJS.AES.encrypt(
+        req.body.password,
+        "secret key 123"
+      ).toString(),
+    });
     await u.save();
     res.status(200).json({ success: "success" });
   } else {
